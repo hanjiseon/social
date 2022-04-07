b74f65624d7f1b87011a5a964c4a79d805b94fc5
JiSeon Han <han1459@cu.ac.kr>
2022-04-07 09:37:29 +0900

    aa

--- social20220324.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-htmlString = '''<html>
-<head><title>My Document</title></head>
-<body>Main text.</body></html>
-'''
-soup = BeautifulSoup(htmlString, "html.parser")
-text = soup.get_text()
-print(text)
